ad65a9e4a359ce43d8e2a665f02b6b41bdc2e9b3
majdajroudi <61711692+majdajroudi@users.noreply.github.com>
2022-03-27 09:53:01 +0300

    Updates

---
+++ precache-manifest.04d20e1c6229ee57da0f0b4259c04f31.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a101ef5980838e414f5091d879360f0d",
+    "url": "/capstone-istanbul-juniper/index.html"
+  },
+  {
+    "revision": "10df2c8963cf9fae351f",
+    "url": "/capstone-istanbul-juniper/static/css/2.1e811c7b.chunk.css"
+  },
+  {
+    "revision": "685d1ed77f6c90a0b0c8",
+    "url": "/capstone-istanbul-juniper/static/css/main.3ec4e18b.chunk.css"
+  },
+  {
+    "revision": "10df2c8963cf9fae351f",
+    "url": "/capstone-istanbul-juniper/static/js/2.54e31822.chunk.js"
+  },
+  {
+    "revision": "89a22e5dc00758f042f0b35ebedf56f1",
+    "url": "/capstone-istanbul-juniper/static/js/2.54e31822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685d1ed77f6c90a0b0c8",
+    "url": "/capstone-istanbul-juniper/static/js/main.782dbab9.chunk.js"
+  },
+  {
+    "revision": "9cea8ac09f749f4dfa5f",
+    "url": "/capstone-istanbul-juniper/static/js/runtime-main.92af9b4a.js"
+  },
+  {
+    "revision": "d8b17089388b6baeef6330b3808bf4c2",
+    "url": "/capstone-istanbul-juniper/static/media/acidRain.d8b17089.svg"
+  },
+  {
+    "revision": "d7becd29c8feb0045647d3984be799e7",
+    "url": "/capstone-istanbul-juniper/static/media/airPollution.d7becd29.svg"
+  },
+  {
+    "revision": "ac89569fcf3964a676cf7e0bef4be9ce",
+    "url": "/capstone-istanbul-juniper/static/media/alternateEnergy.ac89569f.svg"
+  },
+  {
+    "revision": "056b1cbb0e0c62ffede07bdf02aac2fd",
+    "url": "/capstone-istanbul-juniper/static/media/blue-spot.056b1cbb.svg"
+  },
+  {
+    "revision": "d2727124aee64e5476a6557ee15b1765",
+    "url": "/capstone-istanbul-juniper/static/media/chemicalWaste.d2727124.svg"
+  },
+  {
+    "revision": "36563237d0f9cf9873b30a2c39561792",
+    "url": "/capstone-istanbul-juniper/static/media/child.36563237.png"
+  },
+  {
+    "revision": "2766957a3b8c7cf9b6469b1272eaa452",
+    "url": "/capstone-istanbul-juniper/static/media/child_2.2766957a.png"
+  },
+  {
+    "revision": "4f5728e96bd36c01eb3bb6db96d5b2f4",
+    "url": "/capstone-istanbul-juniper/static/media/conservation.4f5728e9.jpg"
+  },
+  {
+    "revision": "1ab5ad822748770ddc16b166372e3206",
+    "url": "/capstone-istanbul-juniper/static/media/deforestation.1ab5ad82.svg"
+  },
+  {
+    "revision": "03d7ba71403c47c72b820635d1470919",
+    "url": "/capstone-istanbul-juniper/static/media/earthNav.03d7ba71.svg"
+  },
+  {
+    "revision": "891171e50ae32db3a2a26fb502fc5103",
+    "url": "/capstone-istanbul-juniper/static/media/earthResourceSetion.891171e5.svg"
+  },
+  {
+    "revision": "6240610f6ba6c31cfbfd47d08cc0b082",
+    "url": "/capstone-istanbul-juniper/static/media/family.6240610f.png"
+  },
+  {
+    "revision": "53c17f36c6ee377c89da4e4c6d1a66e1",
+    "url": "/capstone-istanbul-juniper/static/media/footerBg.53c17f36.svg"
+  },
+  {
+    "revision": "312ff5f9863ae06ee233f6735afe6c13",
+    "url": "/capstone-istanbul-juniper/static/media/globalWarming.312ff5f9.svg"
+  },
+  {
+    "revision": "4069f2476efcc9fa01b7783b56de16bd",
+    "url": "/capstone-istanbul-juniper/static/media/greenhouseEffect.4069f247.svg"
+  },
+  {
+    "revision": "fe554827ae74c080e9dd7883345034c7",
+    "url": "/capstone-istanbul-juniper/static/media/habitat.fe554827.jpg"
+  },
+  {
+    "revision": "cba93ff557c5060d9d1b1c36b9c84a0d",
+    "url": "/capstone-istanbul-juniper/static/media/holdingPlant.cba93ff5.jpg"
+  },
+  {
+    "revision": "1a5be7978e1be68f80c57a0c2ebc6252",
+    "url": "/capstone-istanbul-juniper/static/media/kid-1.1a5be797.png"
+  },
+  {
+    "revision": "72fae50f43b387e9fecb24f6fda2cf6c",
+    "url": "/capstone-istanbul-juniper/static/media/kid-2.72fae50f.png"
+  },
+  {
+    "revision": "0c231e9f6767075c6dcde4b99bc1ac01",
+    "url": "/capstone-istanbul-juniper/static/media/littering.0c231e9f.svg"
+  },
+  {
+    "revision": "97df82e98758658a6fd4f5222fe2aab9",
+    "url": "/capstone-istanbul-juniper/static/media/logo.97df82e9.svg"
+  },
+  {
+    "revision": "2b869fbffe5bfef0c03b01b59bac0311",
+    "url": "/capstone-istanbul-juniper/static/media/meltingIceCaps.2b869fbf.svg"
+  },
+  {
+    "revision": "744f08229f292df2990b0169820a6383",
+    "url": "/capstone-istanbul-juniper/static/media/memoryGame.744f0822.svg"
+  },
+  {
+    "revision": "848f7863b299c78ab32733a07992a2fc",
+    "url": "/capstone-istanbul-juniper/static/media/memoryGameFrontImage.848f7863.png"
+  },
+  {
+    "revision": "6b7be6d215445a00bcd6c81fcb054235",
+    "url": "/capstone-istanbul-juniper/static/media/memoryGameSuccessSymbol.6b7be6d2.svg"
+  },
+  {
+    "revision": "d33035423ed1b1d7463f13e0455578dd",
+    "url": "/capstone-istanbul-juniper/static/media/ourClimateChange.d3303542.png"
+  },
+  {
+    "revision": "74d9fde2c555cc9e950b85b6a50d0b92",
+    "url": "/capstone-istanbul-juniper/static/media/ozoneHole.74d9fde2.svg"
+  },
+  {
+    "revision": "8fc5b31ebd25d40490d2053b686eab7b",
+    "url": "/capstone-istanbul-juniper/static/media/planting.8fc5b31e.jpg"
+  },
+  {
+    "revision": "4bf0160590af03670e8db29f9b2c1e83",
+    "url": "/capstone-istanbul-juniper/static/media/pollutionResourceSetion.4bf01605.svg"
+  },
+  {
+    "revision": "2a47c0283fd44facff9212b7b2f4e764",
+    "url": "/capstone-istanbul-juniper/static/media/puzzleGame.2a47c028.svg"
+  },
+  {
+    "revision": "5ce9871dc09d08e5827c582587a1bab5",
+    "url": "/capstone-istanbul-juniper/static/media/quizGame.5ce9871d.svg"
+  },
+  {
+    "revision": "2cc153f7dff660ca5d746193709762e9",
+    "url": "/capstone-istanbul-juniper/static/media/recycle.2cc153f7.svg"
+  },
+  {
+    "revision": "f641ade23dc8e1ac87742cbeaef66b14",
+    "url": "/capstone-istanbul-juniper/static/media/recycleResourceSetion.f641ade2.svg"
+  },
+  {
+    "revision": "f90af85d425960bf31ed96f97f4eaa33",
+    "url": "/capstone-istanbul-juniper/static/media/resourcesForEducation.f90af85d.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/capstone-istanbul-juniper/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/capstone-istanbul-juniper/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/capstone-istanbul-juniper/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/capstone-istanbul-juniper/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "996120b085c8aba58a9e5ae93f3c1354",
+    "url": "/capstone-istanbul-juniper/static/media/waterPollution.996120b0.svg"
+  },
+  {
+    "revision": "d72162dacf529d6a7f941783398e20f4",
+    "url": "/capstone-istanbul-juniper/static/media/whoCausesClimate.d72162da.png"
+  },
+  {
+    "revision": "2b2233e07a33537c91d5feabacd9de30",
+    "url": "/capstone-istanbul-juniper/static/media/world.2b2233e0.svg"
+  },
+  {
+    "revision": "cff3d14f00673914837af7be203b5224",
+    "url": "/capstone-istanbul-juniper/static/media/yellow-spot.cff3d14f.svg"
+  }
+]);
